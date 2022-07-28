80737988f4c6721876a3f425def33918451e27e0
umitavc <umitavc62@gmail.com>
2022-07-28 10:45:50 +0300

    edit done I didn't apply get_it

--- lib/core/init/locator.dart
+++ lib/core/init/locator.dart
@@ -1,6 +1,8 @@
 import 'package:birindirm_deneme/screens/coupons/view_model/coupons_view_model.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../screens/brands_view/viewmodel/brands_view_model.dart';
+
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
